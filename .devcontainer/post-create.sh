07c56113fffe8188dd21134d8d7c165a0d0e97f5
Chris Hamilton <chris@tracktownsoftware.com>
2023-03-21 09:54:01 -0700

    removed 'dotnet build' from post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,4 +4,4 @@ dotnet dev-certs https
 sudo -E dotnet dev-certs https -ep /usr/local/share/ca-certificates/aspnet/https.crt --format PEM
 
 ## Restore .NET packages and build the default solution
-dotnet restore && dotnet build
+dotnet restore
